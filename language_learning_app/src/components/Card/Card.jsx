ed3ebe42819822b300523f3304a8b95493cd7dc1
RenHayakawa <lizok_k1998@mail.ru>
2023-04-02 23:43:12 +0300

    Week 34: Added focus on the button when new Card appears

--- language_learning_app/src/components/Card/Card.jsx
+++ language_learning_app/src/components/Card/Card.jsx
@@ -1,15 +1,20 @@
 import CSSModules from 'react-css-modules';
 import style from '../Card/card.module.scss';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function Card({ data, getWordsNumber }) {
     const { english, transcription, russian } = data;
     const [isTranslate, setIsTraslate] = useState(false);
+    const refButton = useRef();
 
     function getTranslate() {
         setIsTraslate(!isTranslate);
         getWordsNumber();
     }
+
+    useEffect(() => {
+        refButton.current.focus()
+    }, [])
 
     return (
         <div styleName='container'>
@@ -18,7 +23,7 @@ function Card({ data, getWordsNumber }) {
             {
                 isTranslate ?
                     <div styleName='russian'>{russian}</div> :
-                    <button onClick={getTranslate}>проверить</button>
+                    <button onClick={getTranslate} ref={refButton}>проверить</button>
             }
         </div>
     )
